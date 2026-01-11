8038aa501892d922bbb244a9f0729cad00d229d3
AIAdmin <aiadmin@gravitydrive>
2026-01-11 17:38:46 -0600

    resetting

--- sdnext-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LOGDIR="/opt/apps/neso/logs"
-mkdir -p "$LOGDIR"
-TIMESTAMP=$(date +%Y%m%d_%H%M%S)
-LOGFILE="$LOGDIR/sdnext_${TIMESTAMP}.log"
-
-cd /opt/apps/neso/sd-next
-./webui.sh --listen --server-name 0.0.0.0 --port 7860 --api --use-rocm 2>&1 | tee -a "$LOGFILE"
